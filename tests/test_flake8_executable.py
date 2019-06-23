e926cac086d3140f747d32cd050c3103a18f6152
Hong Xu <hong@topbug.net>
2019-06-23 00:06:51 -0700

    Restructure test folder.

--- tests/test_flake8_executable.py
+++ tests/test_flake8_executable.py
@@ -23,12 +23,14 @@ from flake8_executable import ExecutableChecker, EXE001, EXE002
 
 class Flake8ExecutableTestCase(unittest.TestCase):
 
+    _python_files_folder = 'to-be-tested/'
+
     @parameterized.expand([
         (EXE001, 'exe001'),
         (EXE002, 'exe002')])
     def test_exe_positive(self, error, error_code):
         "Test cases in which an error should be reported."
-        filename = Path(__file__).absolute().parent / (error_code + '_pos.py')
+        filename = Path(__file__).absolute().parent / (self._python_files_folder + error_code + '_pos.py')
         ec = ExecutableChecker(filename=str(filename))
         errors = tuple(ec.run())
         self.assertEqual(errors, (error,))
@@ -38,7 +40,7 @@ class Flake8ExecutableTestCase(unittest.TestCase):
         'exe002'])
     def test_exe_negative(self, error_code):
         "Test cases in which no error should be reported."
-        filename = Path(__file__).absolute().parent / (error_code + '_neg.py')
+        filename = Path(__file__).absolute().parent / (self._python_files_folder + error_code + '_neg.py')
         ec = ExecutableChecker(filename=str(filename))
         errors = tuple(ec.run())
         self.assertFalse(errors)
