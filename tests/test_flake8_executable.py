28f462e5d006a802fa86e265431b3cae85bfbf05
Hong Xu <hong@topbug.net>
2019-06-23 19:00:42 -0700

    Refactor test: Avoid redundant code for obtaining filenames.

--- tests/test_flake8_executable.py
+++ tests/test_flake8_executable.py
@@ -25,7 +25,17 @@ from flake8_executable import ExecutableChecker, exe001, exe002, exe003
 
 class Flake8ExecutableTestCase(unittest.TestCase):
 
-    _python_files_folder = 'to-be-tested/'
+    _python_files_folder = Path(__file__).absolute().parent / 'to-be-tested'
+
+    @classmethod
+    def _get_pos_filename(cls, error_code):
+        "Get the filename for which an error of error_code should be emitted."
+        return cls._python_files_folder / (error_code + '_pos.py')
+
+    @classmethod
+    def _get_neg_filename(cls, error_code):
+        "Get the filename for which an error of error_code should not be emitted."
+        return cls._python_files_folder / (error_code + '_neg.py')
 
     @parameterized.expand([
         (exe001, 'exe001', {}),
@@ -33,7 +43,7 @@ class Flake8ExecutableTestCase(unittest.TestCase):
         (exe003, 'exe003', {'shebang': '#!/bin/bash'})])
     def test_exe_positive(self, error, error_code, params):
         "Test cases in which an error should be reported."
-        filename = Path(__file__).absolute().parent / (__class__._python_files_folder + error_code + '_pos.py')
+        filename = __class__._get_pos_filename(error_code)
         ec = ExecutableChecker(filename=str(filename))
         errors = tuple(ec.run())
         self.assertEqual(errors, (error(**params),))
@@ -44,7 +54,7 @@ class Flake8ExecutableTestCase(unittest.TestCase):
         'exe003'])
     def test_exe_negative(self, error_code):
         "Test cases in which no error should be reported."
-        filename = Path(__file__).absolute().parent / (__class__._python_files_folder + error_code + '_neg.py')
+        filename = __class__._get_neg_filename(error_code)
         ec = ExecutableChecker(filename=str(filename))
         errors = tuple(ec.run())
         self.assertFalse(errors)  # errors should be empty
@@ -60,7 +70,7 @@ class Flake8ExecutableTestCase(unittest.TestCase):
         (exe003, 'exe003', {'shebang': '#!/bin/bash'})])
     def test_stdin_positive(self, error, error_code, params):
         "Test case in which an error should be reported (input is stdin)."
-        filename = Path(__file__).absolute().parent / (__class__._python_files_folder + error_code + '_pos.py')
+        filename = __class__._get_pos_filename(error_code)
         errors = __class__._run_checker_stdin_from_file(filename)
         self.assertEqual(errors, (error(**params),))
 
@@ -70,7 +80,7 @@ class Flake8ExecutableTestCase(unittest.TestCase):
         'exe003'])
     def test_stdin_negative(self, error_code):
         "Test cases in which no error should be reported (input is stdin)."
-        filename = Path(__file__).absolute().parent / (__class__._python_files_folder + error_code + '_neg.py')
+        filename = __class__._get_neg_filename(error_code)
         errors = __class__._run_checker_stdin_from_file(filename)
         self.assertFalse(errors)  # errors should be empty
 
@@ -79,7 +89,7 @@ class Flake8ExecutableTestCase(unittest.TestCase):
         'exe002'])
     def test_stdin_negative_otherwise_positive(self, error_code):
         "Test errors that should not be emitted when the input is from stdin, even if they should be otherwise emitted."
-        filename = Path(__file__).absolute().parent / (__class__._python_files_folder + error_code + '_pos.py')
+        filename = __class__._get_pos_filename(error_code)
         errors = __class__._run_checker_stdin_from_file(filename)
         self.assertFalse(errors)  # errors should be empty
 
