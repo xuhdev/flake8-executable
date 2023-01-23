6e4cd142e5fad21a4a1394da150e3bd23551282d
Hong Xu <hong@topbug.net>
2023-01-22 16:02:51 -0800

    Add python 3.11 support

--- flake8_executable/__init__.py
+++ flake8_executable/__init__.py
@@ -20,7 +20,7 @@ import os
 import re
 from typing import Any, Iterable, List, Tuple, Optional, Union
 
-from ._version import version as __version__
+from ._version import version as __version__  # type: ignore
 
 __all__ = ('__version__',
            'ExcutableChecker',
